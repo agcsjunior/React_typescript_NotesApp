0d13e025a59cc1e5a218f29356d8eebab13d2638
Debasish Mahapatra <agcsdebasish@gmail.com>
2023-12-23 23:46:13 +0530

    updating Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css"
+import { Routes,Route, Navigate } from "react-router-dom"
+
 function App() {
 
   return (
-      <h1>Hi Boys hat are you doing </h1>
+      <Routes>
+        <Route path = "/" element = {<h1> Home</h1>} />
+        <Route path = "/about" element = {<h1> about</h1>} />
+        <Route path = "*" element = {<Navigate to = "/" />} />
+      </Routes>
        
   )
 }
